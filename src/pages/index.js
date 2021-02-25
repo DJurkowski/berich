c02e860e52223c8567686021e132488ecf069871
Dominik <jurkowski.dominik.andrzej@gmail.com>
2021-02-25 20:38:56 +0100

    Add open/close system for sidebar navigation

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Navbar from '../components/Navbar';
+import Sidebar from '../components/Navbar/Sidebar';
 
 const Home = () => {
+
+    const [ isOpen, setIsOpen ] = useState(false);
+
+    const toggle = () => {
+        setIsOpen(!isOpen);
+    };
+
     return (
-        <div>
-            HomePAge babe!
-        </div>
+        <>
+            <Navbar toggle={setIsOpen}/>
+            <Sidebar isOpen={isOpen} toggle={toggle}/>
+        </>
     );
 };
 

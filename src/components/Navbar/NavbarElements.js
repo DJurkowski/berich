291a9a71664b3fada6bfa196d053ba4fd213b80f
Dominik <jurkowski.dominik.andrzej@gmail.com>
2021-02-25 20:39:42 +0100

    Change black color for custom black

--- src/components/Navbar/NavbarElements.js
+++ src/components/Navbar/NavbarElements.js
@@ -4,7 +4,7 @@ import { Link as LinkS } from 'react-scroll';
 
 export const Nav = styled.nav`
     /* background: ${({scrollNav}) => (scrollNav ? '#000' : 'transparent')}; */
-    background: #000;
+    background: #0d0d0d;
     height: 80px;
     /* margin-top: -80px; */
     display: flex;

99b5298946e60d1263f188bca20d5162025af5c3
Dominik <jurkowski.dominik.andrzej@gmail.com>
2021-02-28 13:46:46 +0100

    Add action which get 4 most popular coins

---
+++ src/actions/infocoins.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+import { GET_INFO_COINS, INFO_COINS_ERROR } from './types';
+
+export const getCoins = () => async dispatch => {
+    try {
+        const res = await axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=4&page=1&sparkline=false');
+
+        dispatch({
+            type: GET_INFO_COINS,
+            payload: res.data
+        })
+
+    } catch (err) {
+        console.log(err);
+    }
+};

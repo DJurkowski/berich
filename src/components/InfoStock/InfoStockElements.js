a2528f36eddd2c4d739b86f2e49c1ed02be833a5
Dominik <jurkowski.dominik.andrzej@gmail.com>
2021-03-01 23:25:25 +0100

    Add styling for info stock containter

---
+++ src/components/InfoStock/InfoStockElements.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const InfoStockContainer = styled.div`
+    position: absolute;
+    bottom: -130px;
+    right: (100% - 700px);
+    background: #fff;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    border-radius: 7px;
+    z-index: 3;
+
+    box-shadow: 1px 7px 5px 6px rgba(0,0,0,0.14);
+-webkit-box-shadow: 1px 7px 5px 6px rgba(0,0,0,0.14);
+-moz-box-shadow: 1px 7px 5px 6px rgba(0,0,0,0.14);
+`;

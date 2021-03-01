94c4159cd69d440834a018a8cef231e662d2f5de
Dominik <jurkowski.dominik.andrzej@gmail.com>
2021-03-01 23:31:12 +0100

    Add dedicated for every coin component

---
+++ src/components/InfoStock/Coin/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Chart from '../Chart';
+import { CoinContainer, CoinIcon, CoinName, CoinShortName, CoinPrice, CoinPriceChange, CoinChart } from './CoinElements';
+
+const Coin = ({ coin }) => {
+
+    return (
+        <>
+        <CoinContainer key={coin.id}>
+            <CoinIcon src={coin.image} />
+            <CoinName>{coin.name} <CoinShortName>{coin.symbol}</CoinShortName> </CoinName>
+            
+            <CoinPrice>${coin.current_price}</CoinPrice>
+            <CoinPriceChange price={coin.price_change_percentage_24h}>{coin.price_change_percentage_24h.toFixed(2)}%</CoinPriceChange>
+            
+            <Chart coin={coin} />
+        </CoinContainer>
+        </>
+    );
+};
+
+export default Coin;

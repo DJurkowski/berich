13399715fe738b01ccee25b3bf0224929531d3b5
Dominik <jurkowski.dominik.andrzej@gmail.com>
2021-03-04 17:22:34 +0100

    Add services componnet into main page

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import Sidebar from '../components/Navbar/Sidebar';
 import HeroSection from '../components/HeroSection';
 import UniversalSection from '../components/UniversalSection';
 import { aboutSection, cryptoSection } from '../utils/Data/Data';
+import Services from '../components/Services';
 
 const Home = () => {
 
@@ -16,10 +17,11 @@ const Home = () => {
     return (
         <>
             <Navbar toggle={setIsOpen} isHomePage={true}/>
-            <Sidebar isOpen={isOpen} toggle={toggle}/>
+            <Sidebar isOpen={isOpen} toggle={toggle} isHomePage={true}/>
             <HeroSection />
             <UniversalSection {...aboutSection} />
             <UniversalSection {...cryptoSection} />
+            <Services />
         </>
     );
 };

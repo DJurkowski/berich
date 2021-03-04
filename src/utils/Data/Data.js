f2524c77296de235f51d93531da3a28ee9da21c7
Dominik <jurkowski.dominik.andrzej@gmail.com>
2021-03-04 13:56:56 +0100

    Add changebale data for about section

---
+++ src/utils/Data/Data.js
@@ -0,0 +1,19 @@
+import img1 from '../../images/AboutSection/aboutsection.svg';
+
+export const aboutSection = {
+    id: "about",
+    lightBg: true,
+    lightText: false,
+    lightTextDesc: false,
+    topLine: 'Real-Time Crypto Prices',
+    headline: 'Blockchain Data is in Our DNA',
+    description: 'Get the latest prices and charts along with key market signals.',
+    buttonLabel: 'Learn More',
+    imgStart: false,
+    img: img1,
+    alt: 'Cos',
+    dark: false,
+    dark2: true,
+    primary: false,
+    darkText: true
+};

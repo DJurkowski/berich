8503298256565cc02e39e4104e08424c4aec9085
Dominik <jurkowski.dominik.andrzej@gmail.com>
2021-03-04 14:28:26 +0100

    Add data for crypto section

--- src/utils/Data/Data.js
+++ src/utils/Data/Data.js
@@ -1,4 +1,5 @@
 import img1 from '../../images/AboutSection/aboutsection.svg';
+import img2 from '../../images/CryptoSection/cryptosection.svg';
 
 export const aboutSection = {
     id: "about",
@@ -8,12 +9,30 @@ export const aboutSection = {
     topLine: 'Real-Time Crypto Prices',
     headline: 'Blockchain Data is in Our DNA',
     description: 'Get the latest prices and charts along with key market signals.',
-    buttonLabel: 'Learn More',
+    buttonLabel: 'See Stock',
     imgStart: false,
     img: img1,
-    alt: 'Cos',
+    alt: 'About',
     dark: false,
     dark2: true,
     primary: false,
     darkText: true
+};
+
+export const cryptoSection = {
+    id: "crypto",
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'Cryptocurrency is the Future',
+    headline: 'What to Know About Cryptocurrency',
+    description: 'Cryptocurrency is digital money. That means there’s no physical coin or bill — it’s all online. You can transfer cryptocurrency to someone online without a go-between, like a bank. Bitcoin and Ether are well-known cryptocurrencies, but new cryptocurrencies continue to be created.',
+    buttonLabel: 'Get started',
+    imgStart: true,
+    img: img2,
+    alt: 'Crypto',
+    dark: true,
+    dark2: true,
+    primary: true,
+    darkText: false
 };

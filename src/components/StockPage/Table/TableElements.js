67b9545728683e15918040e5c70d6b1f1f90060c
Dominik <jurkowski.dominik.andrzej@gmail.com>
2021-03-10 19:09:43 +0100

    Add stock table with styling

---
+++ src/components/StockPage/Table/TableElements.js
@@ -0,0 +1,50 @@
+import styled from 'styled-components';
+
+export const TableCointainer = styled.table`
+    width: 100%;
+    border: none;
+    border-spacing: 0;
+    padding: 0 40px;
+`;
+
+export const TableHeader = styled.thead``;
+
+export const TableTHContainer = styled.tr``;
+
+export const TableBody = styled.tbody`
+    border: none;
+`;
+
+export const TableTH = styled.th`
+    padding-top: 12px;
+    padding-bottom: 12px;
+    color: white;
+    text-align: center;
+
+    @media screen and (max-width: 530px) {
+        font-size: 14px;
+    }
+`;
+
+export const THMartketCap = styled(TableTH)`
+    
+    @media screen and (max-width: 800px) {
+        display: none;
+    }
+`;
+
+export const THVolume = styled(TableTH)`
+
+    @media screen and (max-width: 650px) {
+        display: none;
+    }
+`;
+
+export const THSymbol = styled(TableTH)`
+
+    @media screen and (max-width: 460px) {
+        display: none;
+    }
+`;
+
+
---
+++ src/components/StockPage/Table/index.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import Coin from '../Coin';
+import { TableCointainer, TableHeader, TableTHContainer, TableBody, TableTH, THMartketCap, THVolume, THSymbol } from './TableElements';
+
+const Table = ({ coins }) => {
+    return (
+        <TableCointainer cellspacing="0" cellpadding="0">
+            <TableHeader>
+                <TableTHContainer>
+                    <TableTH></TableTH>
+                    <TableTH>Name</TableTH>
+                    <THSymbol>Symbol</THSymbol>
+                    <TableTH>Price</TableTH>
+                    <THVolume>Volume</THVolume>
+                    <TableTH>Percent Change</TableTH>
+                    <THMartketCap>Market Cap</THMartketCap>
+                </TableTHContainer>
+            </TableHeader>
+            <TableBody cellspacing="0" cellpadding="0">
+                {coins.map( coin => 
+                    <Coin
+                        key={coin.id}
+                        img={coin.image}
+                        name={coin.name}
+                        symbol={coin.symbol}
+                        price={coin.current_price}
+                        marketCap={coin.market_cap}
+                        priceChange={coin.price_change_percentage_24h}
+                        volume={coin.total_volume}
+                    />    
+                )}
+            </TableBody>
+        </TableCointainer>
+    );
+};
+
+export default Table;

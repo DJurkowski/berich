bdf7055670095fd06783fa37c452bf4fe4ec25a3
Dominik <jurkowski.dominik.andrzej@gmail.com>
2021-03-10 19:06:32 +0100

    Add nav route element

--- src/components/Navbar/NavbarElements.js
+++ src/components/Navbar/NavbarElements.js
@@ -91,6 +91,21 @@ export const NavLinks = styled(LinkS)`
     }
 `;
 
+export const NavRoute = styled(LinkR)`
+    color: #fff;
+    display: flex;
+    align-items: center;
+    font-weight: bold;
+    text-decoration: none;
+    padding: 0 1rem;
+    height: 100%;
+    cursor: pointer;
+
+    &.active {
+        border-bottom: 3px solid #06DE9C;
+    }
+`;
+
 export const NavBtn = styled.nav`
     display: flex;
     align-items: center;

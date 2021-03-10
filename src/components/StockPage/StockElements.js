293eaf076752091da6fa4001903390181614b044
Dominik <jurkowski.dominik.andrzej@gmail.com>
2021-03-10 22:25:04 +0100

    Add styling for toggle

--- src/components/StockPage/StockElements.js
+++ src/components/StockPage/StockElements.js
@@ -4,7 +4,7 @@ export const StockContainer = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
-    margin-top: 20px;
+    padding-top: 30px;
     background-color: ${({dark}) => (dark ? '#010606' : '#f9f9f9')};
     color: ${({dark}) => (dark ? '#fff' : '#010606')};
     min-height: 100vh;
@@ -17,4 +17,17 @@ export const StockH1 = styled.h1`
     @media screen and (max-width: 480px) {
         font-size: 2rem;
     }
+`;
+
+export const ToggleWrappper = styled.div`
+    align-self: flex-end;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-right: 20px;
+`;
+
+export const ToggleLabel = styled.div`
+    display: inline-block;
+    margin-right: 10px;
 `;

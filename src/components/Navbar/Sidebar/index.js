e374d8a1d5ba02b7716932eed2a544e651c9e09d
Dominik <jurkowski.dominik.andrzej@gmail.com>
2021-03-11 20:06:59 +0100

    Add context and setContext action

--- src/components/Navbar/Sidebar/index.js
+++ src/components/Navbar/Sidebar/index.js
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { Context } from '../../../utils/Context/Context';
 
 import { SidebarContainer, Icon, CloseIcon, SidebarWrapper, SidebarMenu, SidebarLink, SideBtnWrap, SidebarBtnLink, SidebarBtnRoute, SidebarRouteLink } from './SidebarElements';
 
 const Sidebar = ({toggle, isOpen, isHomePage=false}) => {
+
+    const [context, setContext] = useContext(Context);
+    
     return (
         <SidebarContainer isOpen={isOpen} onClick={toggle}>
             <Icon>
@@ -13,22 +17,22 @@ const Sidebar = ({toggle, isOpen, isHomePage=false}) => {
                     {isHomePage ?
                         <SidebarLink to="about" onClick={toggle}>About</SidebarLink>
                     :
-                        <SidebarRouteLink to="/" >About</SidebarRouteLink>
+                        <SidebarRouteLink to="/" onClick={() => setContext('about')}>About</SidebarRouteLink>
                     }
                     {isHomePage ?
                         <SidebarLink to="crypto" onClick={toggle}>Crypto</SidebarLink>
                     :
-                        <SidebarRouteLink to="/" >Crypto</SidebarRouteLink>
+                        <SidebarRouteLink to="/" onClick={() => setContext('cryto')}>Crypto</SidebarRouteLink>
                     }
                     {isHomePage ?
                         <SidebarLink to="knowhow" onClick={toggle}>KnowHow</SidebarLink>
                     :
-                        <SidebarRouteLink to="/" >KnowHow</SidebarRouteLink>
+                        <SidebarRouteLink to="/" onClick={() => setContext('knowhow')}>KnowHow</SidebarRouteLink>
                     }
                     {isHomePage ?
                         <SidebarLink to="more" onClick={toggle}>More</SidebarLink>
                     :
-                        <SidebarRouteLink to="/" >More</SidebarRouteLink>
+                        <SidebarRouteLink to="/" onClick={() => setContext('more')}>More</SidebarRouteLink>
                     }
 
                     <SidebarRouteLink to="/stock" >Stock</SidebarRouteLink>
@@ -38,9 +42,8 @@ const Sidebar = ({toggle, isOpen, isHomePage=false}) => {
                         {isHomePage ?
                             <SidebarBtnLink to="hireme" onClick={toggle}>Hire me</SidebarBtnLink>
                         :
-                            <SidebarBtnRoute to="/">Hire me</SidebarBtnRoute>
+                            <SidebarBtnRoute to="/" onClick={() => setContext('hireme')}>Hire me</SidebarBtnRoute>
                         }
-                        
                     </SideBtnWrap>
             </SidebarWrapper>
         </SidebarContainer>

9f05396abb5b32bfaee73dec464b21a4883b7222
Dominik <jurkowski.dominik.andrzej@gmail.com>
2021-03-11 20:07:31 +0100

    Delete unnecessary line of code

--- src/components/Navbar/NavbarElements.js
+++ src/components/Navbar/NavbarElements.js
@@ -2,9 +2,6 @@ import styled from 'styled-components';
 import { Link as LinkR } from 'react-router-dom';
 import { Link as LinkS } from 'react-scroll';
 
-const activeClassName = "nav-item-active";
-
-
 export const Nav = styled.nav`
     background: ${({scrollNav, isHomePage}) => (isHomePage ? (scrollNav ? '#0d0d0d' : 'transparent'): '#0d0d0d' )};
     height: 80px;

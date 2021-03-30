7b1d311224aa1ddf3d5cfdd3e937efc1a8b502cc
Dominik <jurkowski.dominik.andrzej@gmail.com>
2021-03-30 11:55:54 +0200

    Add function which controls redirection to coin details page

--- src/components/StockPage/Coin/index.js
+++ src/components/StockPage/Coin/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import { CoinContainer, CoinImg, CoinName, CoinSymbol, CoinPrice, CoinVolume, CoinPercent, CoinMarketCap, CoinImageWrap } from './CoinElements';
 
-const Coin = ({ img, name, symbol, price, volume, priceChange, marketCap, isDarkBg }) => {
+const Coin = ({ id, img, name, symbol, price, volume, priceChange, marketCap, isDarkBg }) => {
+
+    const history = useHistory();
+
+    const handleClick = () => {
+
+        history.push(`/stock/${id}`);
+    };
 
     return (
-        <CoinContainer isDarkBg={isDarkBg}>
+        <CoinContainer isDarkBg={isDarkBg} onClick={handleClick}>
             
             <CoinImageWrap>
                 <CoinImg src={img} alt={name}/>

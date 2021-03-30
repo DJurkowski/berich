31c2dbacaf129a1f47f9a09f90a2a8c3dbe48338
Dominik <jurkowski.dominik.andrzej@gmail.com>
2021-03-30 11:58:35 +0200

    Add implementation for details action

---
+++ src/actions/details.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+import { GET_COIN_DETAILS, COIN_DETAILS_ERROR } from './types';
+
+export const getDetails = (id) => async dispatch => {
+
+    try {
+        
+        const res = await axios.get(`https://api.coingecko.com/api/v3/coins/${id}?localization=en&tickers=true&market_data=true&community_data=true&developer_data=true&sparkline=true`);
+
+        dispatch({
+            type: GET_COIN_DETAILS,
+            payload: res.data
+        });
+    } catch (err) {
+        console.log(err);
+    }
+};

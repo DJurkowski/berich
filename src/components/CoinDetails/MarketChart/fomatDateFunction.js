4bc2c7fb9ea0fe5905a2f50bfc43641f40855954
Dominik <jurkowski.dominik.andrzej@gmail.com>
2021-04-13 21:37:28 +0200

    Delete unnecessary function

--- src/components/CoinDetails/MarketChart/fomatDateFunction.js
+++
@@ -1,6 +0,0 @@
-export function formatDate(value, timestamp) {
-    const date = new Date(timestamp);
-    const months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
-
-    return (date.getDate() + ' ' + months[date.getMonth()] + ' ' + date.getFullYear);
-}

bd1c46c5b5a6c658df45eaf048a57111550e980c
Dominik <jurkowski.dominik.andrzej@gmail.com>
2021-04-15 14:44:35 +0200

    Add theme context and provider

--- src/components/Routing/index.js
+++ src/components/Routing/index.js
@@ -4,19 +4,23 @@ import Home from '../../pages/index';
 import Stock from '../../pages/stock';
 import Details from '../../pages/details';
 import { Context } from '../../utils/Context/Context';
+import { ThemeContext } from '../../utils/Context/ThemeContext';
 import Notfound from '../../pages/notfound';
 
 const Routes = () => {
     const [ context, setContext ] = useState('');
+    const [ isDarkTheme, setDarkTheme ] = useState(false);
+
     return (
         <Context.Provider value={[context, setContext]}>
+        <ThemeContext.Provider value={[isDarkTheme, setDarkTheme]}>
             <Switch>
                 <Route exact path="/" component={Home} />
                 <Route exact path="/stock" component={Stock} />
                 <Route exact path="/stock/:id" component={Details} />
-                <Route exact path="/stock/:id" component={Details} />
                 <Route component={Notfound} />
             </Switch>
+        </ThemeContext.Provider>
         </Context.Provider>
     );
 };
